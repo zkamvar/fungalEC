3a391a1342d5819815063721b5134c3d913ff402
Zhian N. Kamvar <zkamvar@gmail.com>
2018-03-12 15:25:04 -0500

    Create install.R

---
+++ install.R
@@ -0,0 +1,6 @@
+install.packages("tidyverse") # for dplyr and ggplot2
+install.packages("drc")
+install.packages("plotrix")
+install.packages("agricolae")
+install.packages("extrafont")
+install.packages("knitr")
